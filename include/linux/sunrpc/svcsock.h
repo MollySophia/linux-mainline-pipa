726bb63bb61f3a608bd53ed604635d68eab88b51
Chuck Lever <chuck.lever@oracle.com>
2023-07-14 14:10:44 -0400

    SUNRPC: Send RPC message on TCP with a single sock_sendmsg() call
    There is now enough infrastructure in place to combine the stream
    record marker into the biovec array used to send each outgoing RPC
    message. The whole message can be more efficiently sent with a
    single call to sock_sendmsg() using a bio_vec iterator.
    
    Note that this also helps with RPC-with-TLS: the TLS implementation
    can now clearly see where the upper layer message boundaries are.
    Before, it would send each component of the xdr_buf in a separate
    TLS record.
    
    Suggested-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- include/linux/sunrpc/svcsock.h
+++ include/linux/sunrpc/svcsock.h
@@ -38,6 +38,8 @@ struct svc_sock {
 	/* Number of queued send requests */
 	atomic_t		sk_sendqlen;
 
+	struct page_frag_cache  sk_frag_cache;
+
 	struct completion	sk_handshake_done;
 
 	struct bio_vec		sk_send_bvec[RPCSVC_MAXPAGES]
